551192854875ab382a24006235e5dbf18bf2e2d7
roychou3207 <roychou3207@gmail.com>
2019-09-10 09:30:28 +0800

    輸入datatable 資料型態形成文件 解決 openoffice 無法開啟檔案問題

---
+++ ReportX/ToReport.cs
@@ -0,0 +1,19 @@
+﻿using ReportX.Rep.Attributes;
+using ReportX.Rep.Model;
+using ReportX.Rep.View;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportX
+{
+    public abstract class ToReport
+    {
+
+    }
+}
